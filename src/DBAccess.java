e9424a545e3e0e0925c3b1b30b660ed4d84b6624
杉山希望 <nontan4061@gmail.com>
2018-01-18 15:43:15 +0900

    refactoring

--- src/DBAccess.java
+++ src/DBAccess.java
@@ -15,7 +15,8 @@ public class DBAccess {
 				Class.forName("com.mysql.jdbc.Driver"); //MYSQl用のJDBCドライバをロード
 
 				// コネクションオブジェクトの生成
-				con = DriverManager.getConnection("jdbc:mysql://localhost:65534/TestDB", "user1", "pass1");
+				// TODO 環境に合わせて変更してください　
+				con = DriverManager.getConnection("jdbc:mysql://localhost:65534/tatsuya", "user1", "pass1");
 																														// ↑httpでのwebアクセス方法に似てる？
 
 		}catch(ClassNotFoundException e) {
@@ -27,104 +28,113 @@ public class DBAccess {
 		}
 		return con;
 	}
+	
 	private void closeConnection(Connection con) {
-	try {
+		try {
+				if(con != null) {
+						con.close();
+				}
+		} catch(SQLException e) {
+				System.out.println("DBアクセス時にエラーが発生しました。");
+				e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<shosekiDBSystem> findAll() {
+		
+		Connection con = createConnection();
+		Statement stmt = null;
+		ResultSet rs = null;
+		shosekiDBSystem data;
+		java.util.ArrayList<shosekiDBSystem> list = new ArrayList<shosekiDBSystem>();
+
+		try {
+			
 			if(con != null) {
-					con.close();
+
+				stmt  = con.createStatement();
+
+				rs = stmt.executeQuery("SELECT * FROM shosekiDB");
+
+				while(rs.next()) {
+					String ISBN = rs.getString("ISBN");
+					String bookname = rs.getString("BOOKNAME");
+					String writer = rs.getString("WRITER");
+					String publisher = rs.getString("PUBLISHER");
+					String year = rs.getString("YEAR");
+					String month = rs.getString("MONTH");
+					String day = rs.getString("DAY");
+					data = new shosekiDBSystem(Integer.parseInt(ISBN),
+												bookname,
+												writer,
+												publisher,
+												Integer.parseInt(year),
+												Integer.parseInt(month),
+												Integer.parseInt(day));
+					list.add(data);
+				}
 			}
-	}catch(SQLException e) {
+		} catch(SQLException e) {
 			System.out.println("DBアクセス時にエラーが発生しました。");
 			e.printStackTrace();
-	}
-	}
-	public ArrayList<shosekiDBSystem> findAll() {
-			Connection con = createConnection();
-			Statement stmt = null;
-			ResultSet rs = null;
-			shosekiDBSystem data;
-			java.util.ArrayList<shosekiDBSystem> list = new ArrayList<shosekiDBSystem>();
-
+		}finally {
 			try {
-					if(con != null) {
-
-							stmt  = con.createStatement();
-
-							rs = stmt.executeQuery("SELECT * FROM shosekiDB");
-
-							while(rs.next()) {
-									String ISBN = rs.getString("ISBN");
-									String bookname = rs.getString("BOOKNAME");
-									String writer = rs.getString("WRITER");
-									String publisher = rs.getString("PUBLISHER");
-									String year = rs.getString("YEAR");
-									String month = rs.getString("MONTH");
-									String day = rs.getString("DAY");
-									data = new shosekiDBSystem(Integer.parseInt(ISBN),
-																bookname,
-																writer,
-																publisher,
-																Integer.parseInt(year),
-																Integer.parseInt(month),
-																Integer.parseInt(day));
-									list.add(data);
-							}
-					}
-			}catch(SQLException e) {
-					System.out.println("DBアクセス時にエラーが発生しました。");
-					e.printStackTrace();
-			}finally {
-					try {
-							if(rs != null) {
-									rs.close();
-							}
-							if(stmt != null) {
-									stmt.close();
-							}
-					}catch(SQLException e) {
-							System.out.println("DBアクセス時にエラーが発生しました。");
-							e.printStackTrace();
-					}
+				if(rs != null) {
+						rs.close();
+				}
+				if(stmt != null) {
+						stmt.close();
+				}
+			} catch(SQLException e) {
+				System.out.println("DBアクセス時にエラーが発生しました。");
+				e.printStackTrace();
 			}
-			closeConnection(con);
-			return list;
+		}
+		closeConnection(con);
+		return list;
 	}
 
 	public int registration(shosekiDBSystem shoseki) {
-			Connection con = createConnection();
-			PreparedStatement pstmt = null;
-			String sql = null;
-			int ret = -1;
+		Connection con = createConnection();
+		PreparedStatement pstmt = null;
+		String sql = null;
+		int ret = -1;
+		
+		try {
+			if(con != null) {
+				sql = "INSERT INTO SHOSEKIDB VALUES(?, ?, ?, ?, ?, ?, ?)";
+
+				pstmt  = con.prepareStatement(sql);
+
+				pstmt.setInt(1,shoseki.getISBN());
+				pstmt.setString(2,shoseki.getBookname());
+				pstmt.setString(3,shoseki.getWriter());
+				pstmt.setString(4,shoseki.getPublisher());
+				pstmt.setInt(5,shoseki.getYear());
+				pstmt.setInt(6,shoseki.getMonth());
+				pstmt.setInt(7,shoseki.getDay());
+
+				ret  = pstmt.executeUpdate();
+			}
+		}catch(SQLException e) {
+			System.out.println("DBアクセス時にエラーが発生しました。");
+			e.printStackTrace();
+		} finally {
+			
 			try {
-					if(con != null) {
-						sql = "INSERT INTO SHOSEKIDB VALUES(?, ?, ?, ?, ?, ?, ?)";
-
-						pstmt  = con.prepareStatement(sql);
-
-						pstmt.setInt(1,shoseki.getISBN());
-						pstmt.setString(2,shoseki.getBookname());
-						pstmt.setString(3,shoseki.getWriter());
-						pstmt.setString(4,shoseki.getPublisher());
-						pstmt.setInt(5,shoseki.getYear());
-						pstmt.setInt(6,shoseki.getMonth());
-						pstmt.setInt(7,shoseki.getDay());
-
-						ret  = pstmt.executeUpdate();
-					}
-			}catch(SQLException e) {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+			} catch(SQLException e) {
 				System.out.println("DBアクセス時にエラーが発生しました。");
 				e.printStackTrace();
-			}finally {
-				try {
-					if(pstmt != null) {
-							pstmt.close();
-					}
-			}catch(SQLException e) {
-					System.out.println("DBアクセス時にエラーが発生しました。");
-					e.printStackTrace();
 			}
-			}
-			closeConnection(con);
-			return ret;
+			
+		}
+		
+		closeConnection(con);
+		
+		return ret;
 
-			}
+	}
 }
